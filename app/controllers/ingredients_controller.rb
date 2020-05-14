34506e210e197b07b99be8790fb0fb6e7e266230
Chris Westerman <chris.westerman8@gmail.com>
2020-05-14 13:49:52 +0100

    added index and show controller methods for ingredients / fixed model validaations

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -4,5 +4,6 @@ class IngredientsController < ApplicationController
   end
 
   def show
+    @ingredient = Ingredient.find(params[:id])
   end
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,7 +2,6 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
 
-  validates :description, presense: true
+  validates :description, presence: true
   validates_uniqueness_of :ingredient_id, :scope => [:cocktail_id]
-
 end
