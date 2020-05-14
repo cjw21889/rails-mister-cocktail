db8cab174166b91314f154b9488e8de8b566c7b6
Chris Westerman <chris.westerman8@gmail.com>
2020-05-14 11:55:43 +0100

    created dose, cocktail, and ingredient models

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :ingredient
+  belongs_to :cocktail
+
+  validates :description, presense: true
+  validates_uniqueness_of :ingredient_id, :scope => [:cocktail_id]
+
+end
