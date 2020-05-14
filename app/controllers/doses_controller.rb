785bb9cad01bd74dd5424da654f3c3e6052a131b
Chris Westerman <chris.westerman8@gmail.com>
2020-05-14 15:03:30 +0100

    added dose controller methods and view to add new dose to cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,16 +1,28 @@
 class DosesController < ApplicationController
-  def index
-  end
 
-  def show
+  def index
+    @doses = Dose.all
   end
 
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
-  def edit
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
-  def _form
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
 
-  resources :cocktails
-  resources :ingredients, only: [:index, :show]
+  resources :cocktails do
+    resources :doses, only: %i[new create]
+  end
+
+  resources :doses, only: [:destroy, :index]
+
+  resources :ingredients, only: %i[index show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
